fc382886655c81a6f303fedc8c3fb843ea46d76f
shumpei <shum125.thriv@gmail.com>
2026-05-02 00:03:54 +0900

    fix

--- dao/db_connection.py
+++ dao/db_connection.py
@@ -19,7 +19,8 @@ def get_connection():
         port=DB_PORT,
         dbname=DB_NAME,
         user=DB_USER,
-        password=DB_PASSWORD
+        password=DB_PASSWORD,
+        sslmode='require'
     )
 
 
